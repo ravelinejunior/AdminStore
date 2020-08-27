66634c220df5e5573bfa977666df77df0daacc56
ravelinejunior <junior.raveline@gmail.com>
2020-08-27 00:41:46 -0300

    ll

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:AdminRavelineStore/screens/login_screen/login_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
